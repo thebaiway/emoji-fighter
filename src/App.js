9bbb3a297b70a730b9b3838ac79a0eb69d0f897a
Bailee <103554611+thebaiway@users.noreply.github.com>
2023-01-26 10:40:47 -0800

    converted to react app

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import React, {useState} from 'react'
+
+
+function App() {
+
+  let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+
+  const [fight, setFight] = useState("vs")
+
+  function fightStage () {
+    setFight(prevFight => prevFight =
+      fighters[emojiOne] + " vs " + fighters[emojiTwo])
+  }
+
+  let emojiOne = Math.floor(Math.random() * fighters.length)
+  let emojiTwo = Math.floor(Math.random() * fighters.length)
+
+  function fightBtn () {
+    fightStage()
+  }
+
+  return (
+    <div className="App">
+        <h1>Emoji<br />Fighter</h1>
+        <div id="stage">{fight}</div>
+        <button id="fightButton" onClick = {fightBtn}>Pick Fighters!</button> 
+    </div>
+  );
+}
+
+export default App;
